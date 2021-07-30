97379425b2ea931745f555ca49a1d90e54b8353c
Kanha70 <k.kanhaiya9724@gmail.com>
2021-07-30 17:40:48 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/viewmodel/MainViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    var  count: Int = 0
+
+    fun increment(){
+        count++
+    }
+}
